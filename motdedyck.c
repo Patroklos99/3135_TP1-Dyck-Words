95e1ffba17dd1376972ea7792a9605b3a007daea
Salcedo Salcedo, Renzo <salcedo_salcedo.renzo@courrier.uqam.ca>
2022-01-24 00:29:27 -0500

    Squelette du fichier .c

--- motdedyck.c
+++ motdedyck.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#define TAILLE_MOT 40
+#define USAGE "\
+Usage: %s [HEIGHT,AREA] <LETTER 1> <LETTER 2> <WORD>\n\
+\n\
+Draws on an ASCII path of dycks word. The dycks word is provided on stdin and\n\
+the result is printed on stdout. The length of the word must not exceed 40 characters.\n\
+\n\
+If no argument is provided, the program prints this help and exit.\n\
+\n\
+Program parameters :\n\
+  HEIGHT                    Optional parameter to calculate the height of the\n\
+                            path under the word of dyck.\n\
+  AREA                      Optional parameter to calculate the height of\n\
+                            the path under the word of dyck.\n\
+  LETTER 1, LETTER 2        Define the alphabet of the word.\n\
+  WORD                      Word of dyck to draw.\n\
+  "
+
+
+struct motDeDyck {
+    char mot[TAILLE_MOT];   //Le mot saisi
+    char haut;              //Caractere ascendant
+    char bas;               //Caractere descendant
+};
+
+enum error {
+    OK                          = 0,
+    ERREUR_ARGUMENTS_INVALIDES  = 1,
+    ERREUR_DONNEES_INVALIDES    = 2,
+    ERREUR_MOT_TROP_LONG        = 3,
+    ERREUR_LETTRE_INTERDITE     = 4,
+    ERREUR_MOT_NON_EQUILIBRE    = 5
+};
 
 int main(int argc, char *argv[]) {
-    printf("argc = %d\n", argc);
-    for (unsigned int i = 0; i < argc; ++i) {
-        printf("argv[%d] = %s\n", i, argv[i]);
-    }
+    printf("Hello world\n"); 	
     return 0;
 }
